5ce73124f4d8390a79164542a2519c9f402d5df0
prettydiff <info@prettydiff.com>
2018-07-09 22:07:15 -0400

    minor compatibility enhancements to ease execution on downstream CI

--- lexers/all.ts
+++ lexers/all.ts
@@ -1,19 +1,22 @@
 /*jslint node:true*/
 /*eslint-env node*/
 /*eslint no-console: 0*/
-const all = function lexers_all(options, callback) {
+const all = function lexers_all(options:any, callback:Function) {
     "use strict";
     const node = {
             fs: require("fs"),
             path: require("path")
         },
         project:string = __dirname.replace(/lexers(\/|\\)?/, "") + node.path.sep;
-    node.fs.readdir(`${project}lexers`, function nodetest_readdir(err, files) {
+    node.fs.readdir(`${project}lexers`, function nodetest_readdir(err:Error, files:string[]) {
         if (err !== null) {
             console.log(err);
             return process.exit(1);
         }
-        files.forEach(function nodetest_readdir_each(value) {
+        if (options.lexerOptions === undefined) {
+            options.lexerOptions = {};
+        }
+        files.forEach(function nodetest_readdir_each(value:string) {
             if ((/(\.js)$/).test(value) === true) {
                 require(`${project}lexers${node.path.sep + value}`);
                 options.lexerOptions[value.replace(".js", "")] = {};
